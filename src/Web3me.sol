8d03ab290b85f0db645695c4720784bd8026ad47
7i7o <mparij@gmail.com>
2022-10-03 08:05:30 -0300

    build: contract first draft

--- src/Web3me.sol
+++ src/Web3me.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.12;
 
-// import IERC721
-// import IERC1155
+import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/IERC1155MetadataURI.sol";
 
 uint8 constant NO_PFP = 0;
 uint8 constant ERC721 = 1;
 uint8 constant ERC1155 = 2;
 
 struct Pfp {
-    uint8 networkId;
+    uint8 chainId;
     uint8 eip;
     address collection;
     uint256 id;
@@ -22,22 +22,23 @@ contract Web3me {
     mapping ( address => Pfp ) private pfps;
     
     function setPfp(uint8 _networkId, uint8 _eip, address _collection, uint256 _id) external {
-        pfps[msg.sender].networkId = _networkId;
+        pfps[msg.sender].chainId = _networkId;
         pfps[msg.sender].eip = _eip;
         pfps[msg.sender].collection = _collection;
         pfps[msg.sender].id = _id;
     }
 
-    function pfp(address _addr) public returns (string memory) {
+    function pfp(address _addr) public view returns (string memory) {
         Pfp storage _pfp = pfps[_addr];
 
         if ( _pfp.eip == NO_PFP ) revert NoPfp();
 
         if ( _pfp.eip == ERC721 ) {
-            // if ( IERC721(_pfp.collection).ownerOf(_pfp.id) == _addr )
-            //     return IERC721(_pfp.collection).tokenURI(_pfp.id);
+            if ( IERC721Metadata(_pfp.collection).ownerOf(_pfp.id) == _addr )
+                return IERC721Metadata(_pfp.collection).tokenURI(_pfp.id);
         } else {
-
+            if ( IERC1155MetadataURI(_pfp.collection).balanceOf(_addr, _pfp.id) > 0 )
+                return IERC1155MetadataURI(_pfp.collection).uri(_pfp.id);
         }
         return '';
     }
